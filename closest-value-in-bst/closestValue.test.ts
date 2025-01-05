83b4ed0d8e3d4b8f257a41305a7446c72a789ec8
EduardoPetrini <eduardo.petrini@outlook.com>
2025-01-05 18:38:53 -0300

    chore: find the closest in BST

---
+++ closest-value-in-bst/closestValue.test.ts
@@ -0,0 +1,29 @@
+import { describe, it } from 'node:test';
+import assert from 'node:assert';
+import { BST } from './BST';
+import { findClosestValue } from './index.s1';
+
+
+let tree = new BST(10);
+tree.init([5, 2, 1, 15, 13, 22, 14]);
+
+describe('find the closest value', () => {
+    it('should find 13 as the closest of 12', () => {
+        const closest = findClosestValue(tree, 12, null);
+
+        assert.equal(closest, 13);
+    });
+
+    it('should find 22 as the closest of 20', () => {
+        const closest = findClosestValue(tree, 20, null);
+
+        assert.equal(closest, 22);
+    });
+
+    it('should find 5 as the closest of 4', () => {
+        const closest = findClosestValue(tree, 4, null);
+
+        assert.equal(closest, 5);
+    });
+
+});
--- closest-value-in-bst/index.s1.ts
+++ closest-value-in-bst/index.s1.ts
@@ -1,5 +1,31 @@
 import { BST } from "./BST";
 
-export const findClosestValue = (tree: BST, target: number) {
 
+const isCloser = (current: number, previous: number, target: number) => {
+    const currentDiff = Math.abs(current) - Math.abs(target);
+    const previousDiff = Math.abs(previous) - Math.abs(target);
+
+    return Math.abs(currentDiff) <= Math.abs(previousDiff);
+}
+
+export const findClosestValue = (tree: BST | null, target: number, closest: number | null): number | null => {
+    if (!tree) {
+        return closest
+    }
+
+    if (!closest) {
+        closest = tree.value;
+    }
+
+    if (isCloser(tree.value, closest, target)) {
+        closest = tree.value;
+    }
+
+    if (target > tree.value) {
+        return findClosestValue(tree.right, target, closest);
+    } else if (target < tree.value) {
+        return findClosestValue(tree.left, target, closest);
+    }
+
+    return tree.value;
 }
